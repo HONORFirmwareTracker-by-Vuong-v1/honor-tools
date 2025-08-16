a0b8e57b852225dbbbf3781f22716a3ec6e70050
VanVuong41429 <vanvuong41429@gmail.com>
2025-08-16 15:35:47 +0700

    Update 01_download_hihonorcdn_xmls.sh

--- hihonorcdn-crawler/01_download_hihonorcdn_xmls.sh
+++ hihonorcdn-crawler/01_download_hihonorcdn_xmls.sh
@@ -3,8 +3,8 @@
 # Script tries to find all update files on update.hihonorcdn.com (filelist.xml & changelog.xml) and
 # stores those files into update_hihonorcdn_com_all_* directory for further processing
 
-from=600000
-to=640000
+from=640000
+to=680000
 
 d="update_hihonorcdn_com_all_$from-$to"
 
