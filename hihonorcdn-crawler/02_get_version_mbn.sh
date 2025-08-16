fe69587f12b2015aed9bb41cf3e63dd36d890071
VanVuong41429 <vanvuong41429@gmail.com>
2025-08-16 15:36:30 +0700

    Update 02_get_version_mbn.sh

--- hihonorcdn-crawler/02_get_version_mbn.sh
+++ hihonorcdn-crawler/02_get_version_mbn.sh
@@ -4,7 +4,7 @@
 # zip files tails in parallel to get VERSION.mbn which contains the actual version of the update
 #
 # Usage:
-# $ ./get_version_mbn.sh update_hihonorcdn_com_all_600000-640000
+# $ ./get_version_mbn.sh update_hihonorcdn_com_all_640000-680000
 
 SEARCH_DIR="$1"
 [ "x$SEARCH_DIR" != 'x' ] || exit 1
